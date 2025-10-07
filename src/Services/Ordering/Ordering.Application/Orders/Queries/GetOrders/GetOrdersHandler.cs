1fd8c7ab93e65003d888640228c84ab1ce980de9
zohidbek <mengliboyevzohidbek@gmail.com>
2025-10-08 00:43:35 +0500

    fixing bug

--- src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersHandler.cs
@@ -15,7 +15,7 @@ namespace Ordering.Application.Orders.Queries.GetOrders
             var orders = await dbContext.Orders
                            .Include(o => o.OrderItems)
                            .OrderBy(o => o.OrderName.Value)
-                           .Skip(pageSize * (pageIndex - 1))
+                           .Skip(pageSize * pageIndex)
                            .Take(pageSize)
                            .ToListAsync(cancellationToken);
 
